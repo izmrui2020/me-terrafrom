837a75fee398bea86ffab95c86315cdc6b7f182f
rr-rr-re <rr.anonymous.2020@gmail.com>
2021-08-14 01:57:44 +0900

    start terraform

--- test1/main.tf
+++ test1/main.tf
@@ -6,4 +6,14 @@ provider "aws" {
 resource "aws_instance" "hello-world" {
   ami           = "ami-0ce107ae7af2e92b5"
   instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+
+  user_data = <<EOF
+#!/bin/bash
+amazon-linux-extras install -y nginx1.12
+systemctl start nginx
+EOF
 }
